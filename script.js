104d8831a15a1d8ca5a95546e4c03858fc686b35
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-09 14:52:30 +0100

    decrease btn added

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+let countEl = document.getElementById("count-el");
+let count = 0;
 
- 
+function increment() {
+    count = count + 1;
+    countEl.innerText = count;
+}
+function decrement() {
+    count = count - 1;
+    countEl.innerText = count;
+}
+
+function save() {
+    console.log(count);
+}
