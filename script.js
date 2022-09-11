a683aa3632092b169ce3145ea725257795f9e6d6
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-11 10:27:47 +0100

    styling done app complete

--- script.js
+++ script.js
@@ -13,10 +13,13 @@ function decrement() {
 
 const reset = () => {
     count = 0;
+    saveEl.textContent = 0;
 };
 
 function save() {
     let countReg = count + " - ";
     saveEl.textContent += countReg;
+    count = 0;
+    countEl.textContent = 0;
     console.log(count);
 }
