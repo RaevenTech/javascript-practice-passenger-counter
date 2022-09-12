614bd37ab93293af1d815873daa37cae4a47af9a
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-12 11:41:20 +0100

    simple calculator

--- peopleCounter/renderErrorMsg/script.js
+++ peopleCounter/renderErrorMsg/script.js
@@ -1,7 +1,9 @@
 let errorMsg = document.getElementById("error");
+// console.log(errorMsg) always check whith console logs
 let msg = "Something went wrong, please try again";
 
 function error() {
+    // console.log("Button clicked") always check whith console logs
     msg = msg;
     errorMsg.textContent = msg;
 }
