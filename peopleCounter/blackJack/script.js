8346af2e35e3d0c08ae15dd4eca0a9598839935a
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-13 17:32:26 +0100

    card values displayed

--- peopleCounter/blackJack/script.js
+++ peopleCounter/blackJack/script.js
@@ -1,6 +1,6 @@
 let firstCard = 10;
 let secondCard = 7;
-let thirdCard = 3;
+let thirdCard = 5;
 let cardCount = firstCard + secondCard + thirdCard;
 
 let hasBlackjack = false;
@@ -9,18 +9,27 @@ let stillActive = true;
 let message = "";
 let messageEl = document.getElementById("message_el");
 let countEl = document.querySelector("#count_el");
+let cardsEl = document.getElementById("cards_el");
 
 function startGame() {
     if (cardCount <= 20) {
+        cardsEl.textContent +=
+            " " + firstCard + " + " + secondCard + " + " + thirdCard;
         countEl.textContent += " " + cardCount;
         message = "Do you want to draw another card?";
         stillActive = true;
     } else if (cardCount === 21) {
+        cardsEl.textContent +=
+            " " + firstCard + " + " + secondCard + " + " + thirdCard;
         countEl.textContent += " " + cardCount;
+
         message = "You have Blackjack!!";
         hasBlackjack = true;
     } else {
+        cardsEl.textContent +=
+            " " + firstCard + " + " + secondCard + " + " + thirdCard;
         countEl.textContent += " " + cardCount;
+
         message = "BUST! You are out of the game!";
         stillActive = false;
     }
