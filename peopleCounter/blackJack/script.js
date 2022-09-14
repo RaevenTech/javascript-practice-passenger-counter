8e5ad4883db9c7960a94401d960a42cfb963a3e9
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-14 12:11:26 +0100

    pushing new cards using methods

--- peopleCounter/blackJack/script.js
+++ peopleCounter/blackJack/script.js
@@ -1,7 +1,8 @@
 let firstCard = 10;
 let secondCard = 7;
-let thirdCard = 5;
-let cardCount = firstCard + secondCard + thirdCard;
+
+let cardCount = firstCard + secondCard;
+let cards = [firstCard, secondCard];
 
 let hasBlackjack = false;
 let stillActive = true;
@@ -12,26 +13,33 @@ let countEl = document.querySelector("#count_el");
 let cardsEl = document.getElementById("cards_el");
 
 function startGame() {
+    renderGame();
+}
+
+function renderGame() {
     if (cardCount <= 20) {
-        cardsEl.textContent +=
-            " " + firstCard + " + " + secondCard + " + " + thirdCard;
-        countEl.textContent += " " + cardCount;
+        cardsEl.textContent = "Cards: " + cards[0] + " + " + cards[1];
+        countEl.textContent = "Count: " + cardCount;
         message = "Do you want to draw another card?";
         stillActive = true;
     } else if (cardCount === 21) {
-        cardsEl.textContent +=
-            " " + firstCard + " + " + secondCard + " + " + thirdCard;
-        countEl.textContent += " " + cardCount;
-
-        message = "You have Blackjack!!";
+        cardsEl.textContent = "Cards: " + firstCard + " + " + secondCard;
+        countEl.textContent = "Count: " + cardCount;
+        message = "Blackjack!! YOU WIN!!";
         hasBlackjack = true;
     } else {
-        cardsEl.textContent +=
-            " " + firstCard + " + " + secondCard + " + " + thirdCard;
-        countEl.textContent += " " + cardCount;
-
+        cardsEl.textContent = "Cards: " + firstCard + " + " + secondCard;
+        countEl.textContent = "Count: " + cardCount;
         message = "BUST! You are out of the game!";
         stillActive = false;
     }
     messageEl.textContent = message;
+}
+
+function newCard() {
+    messageEl.textContent = "Drawing a new card from the deck!";
+    let card = 2;
+    cardCount += card;
+    cards.push(card);
+    renderGame();
 }
