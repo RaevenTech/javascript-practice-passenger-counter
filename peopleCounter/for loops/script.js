d92c943f313a16ea8484de009ec4f1cfc5405305
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-14 15:02:27 +0100

    for loop practice

---
+++ peopleCounter/for loops/script.js
@@ -0,0 +1,24 @@
+// Count to ten!
+
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+//    START           FINISH       STEP SIZE
+for (let count = 1; count < 11; count += 1) {
+    console.log(count);
+}
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+for (let count = 10; count < 101; count + 10) {
+    console.log(count);
+}
+
+let cards = [7, 3, 9];
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i]);
+}
