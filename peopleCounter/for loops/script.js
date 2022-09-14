594cf41e33ba282a11379a87f7917ee1f22f7fce
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-14 16:20:46 +0100

    simple for loops

--- peopleCounter/for loops/script.js
+++ peopleCounter/for loops/script.js
@@ -1,11 +1,8 @@
 // Count to ten!
-
 // We need to specify...
-
 // Where should we START counting?
 // Where is the FINISH line?
 // What's the STEP SIZE we should use?
-
 //    START           FINISH       STEP SIZE
 for (let count = 1; count < 11; count += 1) {
     console.log(count);
@@ -21,4 +18,11 @@ let cards = [7, 3, 9];
 // Use cards.length to specify how long the loop should
 for (let i = 0; i < cards.length; i++) {
     console.log(cards[i]);
+}
+
+let sentence = ["Hello ", "my ", "name ", "is ", "Raeven"];
+let greetingEl = document.getElementById("greeting_el");
+// Render the sentence in the greetingEl paragraph using a for loop and .textContent
+for (let i = [0]; i < sentence.length; i++) {
+    greetingEl.textContent += sentence[i] + " ";
 }
