00809a5a420609858237ff03c4767696401439ae
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-16 14:39:39 +0100

    simple for loop

--- peopleCounter/for loops/script.js
+++ peopleCounter/for loops/script.js
@@ -7,11 +7,14 @@
 for (let count = 1; count < 11; count += 1) {
     console.log(count);
 }
+//------------------------------------------------------------------------------
 
 // Create a for loop that counts from 10 to 100 in steps of 10
-for (let count = 10; count < 101; count + 10) {
-    console.log(count);
+for (let count1 = 10; count1 < 101; count1 += 10) {
+    console.log(count1);
 }
+
+//------------------------------------------------------------------------------
 
 let cards = [7, 3, 9];
 // Create a for loop that logs out all the cards in the array
@@ -19,10 +22,25 @@ let cards = [7, 3, 9];
 for (let i = 0; i < cards.length; i++) {
     console.log(cards[i]);
 }
+//-----------------------------------------------------------------------------
 
 let sentence = ["Hello ", "my ", "name ", "is ", "Raeven"];
 let greetingEl = document.getElementById("greeting_el");
 // Render the sentence in the greetingEl paragraph using a for loop and .textContent
 for (let i = [0]; i < sentence.length; i++) {
     greetingEl.textContent += sentence[i] + " ";
+}
+//-----------------------------------------------------------------------------
+let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
+
+/* Use a for loop to log the following to the console:
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
+for (let i = 0; i < largeCountries.length; i++) {
+    console.log(largeCountries[i]);
 }
