f1148eeac1b8b88aeaed3d0c19b06e67269a16e5
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-17 11:56:34 +0100

    sorting an array

--- peopleCounter/for loops and arrays/script.js
+++ peopleCounter/for loops and arrays/script.js
@@ -64,3 +64,21 @@ largeCountries1.shift();
 largeCountries1.unshift("China");
 
 console.log(largeCountries1);
+
+//-----------------------------------------------------------------------------
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"];
+let appleShelf = document.getElementById("apple_shelf");
+let orangeShelf = document.getElementById("orange_shelf");
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+function fruits() {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+            appleShelf.textContent += "🍎";
+        } else if (fruit[i] === "🍊") {
+            orangeShelf.textContent += "🍊";
+        }
+    }
+}
+fruits();
