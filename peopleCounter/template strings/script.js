c3cffaa9d07d1d0e777d7c3b1cbc66b1e41b4e24
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-21 15:58:00 +0100

    temperate literal

--- peopleCounter/template strings/script.js
+++ peopleCounter/template strings/script.js
@@ -2,7 +2,14 @@ const recipient = "James";
 
 // Refactor the email string below to use template strings
 //const email = "Hey " + recipient + "! How is it going? Cheers Per";
+const sender = "Raeven";
+// Create a new variable, sender, and set its value to your name
+// Use your sender variable instead of "Per"
 
-const email = `Hey ${recipient}! How is it going? Cheers Per.`;
+const email = `
+Hey ${recipient}! 
+How is it going? 
+Cheers ${sender}.
+`;
 
 console.log(email);
