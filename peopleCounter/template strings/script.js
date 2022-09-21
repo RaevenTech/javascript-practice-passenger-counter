1a78793a43d3981771a653eacac5bdbfb9ba67a0
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-21 10:21:04 +0100

    template  literals/strings

---
+++ peopleCounter/template strings/script.js
@@ -0,0 +1,8 @@
+const recipient = "James";
+
+// Refactor the email string below to use template strings
+//const email = "Hey " + recipient + "! How is it going? Cheers Per";
+
+const email = `Hey ${recipient}! How is it going? Cheers Per.`;
+
+console.log(email);
