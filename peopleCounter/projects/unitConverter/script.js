daf507d288533702e44f78959a86adbaffffbb40
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-27 16:57:49 +0100

    grabbing elements

---
+++ peopleCounter/projects/unitConverter/script.js
@@ -0,0 +1,13 @@
+const inputEl = document.getElementById("input_el");
+const converBtn = document.getElementById("convert_btn");
+const mtrToft = document.getElementById("mtr_ft_el");
+const ftToMtr = document.getElementById("ft_mtr_el");
+const ltrToGal = document.getElementById("ltr_gal_el");
+const galToLtr = document.getElementById("gal_ltr_el");
+const kgTolbs = document.getElementById("kg_lbs_el");
+const lbsToKg = document.getElementById("lbs_kg_el");
+
+let inputValue = Number(inputEl.value);
+console.log(inputValue);
+
+converBtn.addEventListener("click", function () {});
