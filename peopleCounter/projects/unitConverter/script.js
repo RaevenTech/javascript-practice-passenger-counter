ba098c6ad10c72962c3efc9ab412945dbd9e6c3a
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-28 11:32:07 +0100

    converter complete

--- peopleCounter/projects/unitConverter/script.js
+++ peopleCounter/projects/unitConverter/script.js
@@ -1,13 +1,36 @@
 const inputEl = document.getElementById("input_el");
 const converBtn = document.getElementById("convert_btn");
-const mtrToft = document.getElementById("mtr_ft_el");
-const ftToMtr = document.getElementById("ft_mtr_el");
-const ltrToGal = document.getElementById("ltr_gal_el");
-const galToLtr = document.getElementById("gal_ltr_el");
-const kgTolbs = document.getElementById("kg_lbs_el");
-const lbsToKg = document.getElementById("lbs_kg_el");
+const lengthEl = document.getElementById("length_el");
+const volumeEl = document.getElementById("volume_el");
+const massEl = document.getElementById("mass_el");
+const clearBtn = document.getElementById("clear_btn");
 
-let inputValue = Number(inputEl.value);
-console.log(inputValue);
+const meterToFeet = 3.281;
+const literToGallon = 0.264;
+const kiloToPound = 2.204;
+let inputValue = "0";
 
-converBtn.addEventListener("click", function () {});
+converBtn.addEventListener("click", function () {
+    let inputValue = inputEl.value;
+    console.log(inputValue);
+    lengthEl.textContent = `${inputValue} meter = ${(
+        inputValue * meterToFeet
+    ).toFixed(3)} feet | ${inputValue} feet = ${(
+        inputValue / meterToFeet
+    ).toFixed(3)} feet`;
+    volumeEl.textContent = `${inputValue} liters = ${(
+        inputValue * literToGallon
+    ).toFixed(3)} gallons | ${inputValue} gallons = ${(
+        inputValue / literToGallon
+    ).toFixed(3)} liters`;
+    massEl.textContent = `${inputValue} kilograms = ${(
+        inputValue * kiloToPound
+    ).toFixed(3)} pounds | ${inputValue} pounds = ${(
+        inputValue / kiloToPound
+    ).toFixed(3)} kilos`;
+});
+
+const clearedValue = 0;
+clearBtn.addEventListener("click", function () {
+    inputEl.innerHTML = "";
+});
